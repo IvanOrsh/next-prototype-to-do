771bf448308f366ff5b2172b867ad32de1a941f8
IvanOrsh <ksironov@mail.ru>
2024-03-06 00:56:57 +0300

    feat: implement drizzle db instance

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+
+import config from "./config";
+import * as schema from "./schema";
+
+const sqlite = new Database(config.DATABASE_URL);
+
+export const db = drizzle(sqlite, {
+  schema,
+  logger: true,
+});
