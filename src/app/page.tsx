1ed4e8e87864f7e782876554e00ffefe1bdf39a2
IvanOrsh <ksironov@mail.ru>
2024-03-06 05:35:22 +0300

    feat: home page styles - wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,28 @@ import { ModeToggle } from "@/components/mode-toggle";
 import SignInButton from "@/components/signin-button";
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <div>
-      <ModeToggle />
-      {session ? <Button>Go to App</Button> : <SignInButton />}
+    <div className="flex h-screen w-screen justify-center items-center">
+      <div className="flex flex-col gap-5 text-center">
+        <ModeToggle />
+
+        <h1>To Do Prototype</h1>
+
+        <div>
+          {session ? (
+            <Button>
+              Go to App <ArrowRightIcon className="ml-2" />
+            </Button>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
