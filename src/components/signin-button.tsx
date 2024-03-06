333f1b43fbb63f0047cd911fb384389300a5f12a
IvanOrsh <ksironov@mail.ru>
2024-03-06 03:33:24 +0300

    feat: implement sign in button

---
+++ src/components/signin-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { Button } from "./ui/button";
+
+export default function SignInButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        Signed in as {session.user?.name}
+        <br />
+        <Button onClick={() => signOut}>Sign out</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      Not signed in
+      <br />
+      <Button onClick={() => signIn()}>Sign in</Button>
+    </div>
+  );
+}
