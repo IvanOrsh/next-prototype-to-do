9be95ed50999add6cebd1d0544652026f662076e
IvanOrsh <ksironov@mail.ru>
2024-03-06 05:08:37 +0300

    fix: default session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,11 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
 // module augmentation
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       name: string;
-    };
+    } & DefaultSession["user"];
   }
 }
