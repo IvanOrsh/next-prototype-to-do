96073a91081e9262c446e53869cddca2a3b04256
IvanOrsh <ksironov@mail.ru>
2024-03-06 05:27:50 +0300

    refactor: sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import SignInButton from "@/components/signin-button";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <ModeToggle />
+      {session ? <Button>Go to App</Button> : <SignInButton />}
     </div>
   );
 }
--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
 import { Button } from "./ui/button";
 
 export default function SignInButton() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <div>
-        Signed in as {session.user?.name}
-        <br />
-        <Button onClick={() => signOut}>Sign out</Button>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      Not signed in
-      <br />
-      <Button onClick={() => signIn()}>Sign in</Button>
-    </div>
-  );
+  return <Button onClick={() => signIn(undefined)}>Sign in</Button>;
 }
