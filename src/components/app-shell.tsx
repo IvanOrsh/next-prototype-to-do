b8c617c6a94cc79367d818a21a4dd09b23db7a6d
IvanOrsh <ksironov@mail.ru>
2024-03-07 12:47:15 +0300

    feat: stub out app shell

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,7 +1,11 @@
+import Header from "./header";
+
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-12">
-      <div className="sm:col-span-12">header</div>
+      <div className="sm:col-span-12">
+        <Header />
+      </div>
       <div className="sm:col-span-3">sidebar</div>
       <div className="sm:col-span-9">{children}</div>
     </div>
