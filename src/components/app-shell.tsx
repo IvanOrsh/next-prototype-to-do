5c6881b9bb29f221163405162fc0700b81fb5228
IvanOrsh <ksironov@mail.ru>
2024-03-07 20:28:34 +0300

    feat: wire up sidebar with app shell

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,14 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
+
 import Header from "./header";
 import Sidebar from "./sidebar";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-12">
       <div className="sm:col-span-12">
         <Header />
       </div>
-      <div className="sm:col-span-3">
-        <Sidebar />
+      <div
+        className={cn(
+          "w-full h-full",
+          "bg-background",
+          "absolute top-20 left-0",
+          "transition delay-200",
+          "sm:col-span-3 sm:relative sm:top-0 sm:left-0 sm:transform-none",
+          "-translate-x-full",
+          open && "translate-x-0"
+        )}
+      >
+        <Sidebar onClick={() => setOpen(false)} />
+      </div>
+
+      <div className="sm:hidden mt-5">
+        <Button
+          className={cn(
+            pathname === "/tasks" && "text-accent-blue-foreground",
+            pathname === "/myday" && "text-accent-green-foreground",
+            pathname === "/important" && "text-accent-pink-foreground"
+          )}
+          variant="link"
+          onClick={() => setOpen(true)}
+        >
+          <ChevronLeftIcon className="w-6 h-6" /> Lists
+        </Button>
       </div>
       <div className="sm:col-span-9">{children}</div>
     </div>
