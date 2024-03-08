ae5c41a7f1665d176127c4d236373fe18bcabe28
IvanOrsh <ksironov@mail.ru>
2024-03-08 23:18:54 +0300

    feat: implement adding a new task

---
+++ src/components/add-task.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { createTask } from "@/actions/create-task";
+
+export default function AddTask() {
+  const [isAdding, setIsAdding] = useState(false);
+  const [title, setTitle] = useState("");
+
+  async function handleKeyDown(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      await createTask(title);
+      setIsAdding(false);
+      setTitle("");
+    }
+  }
+
+  return (
+    <div>
+      {isAdding ? (
+        <Input
+          type="text"
+          name="title"
+          placeholder="Try adding pay utilities by Friday 6pm"
+          onKeyDown={handleKeyDown}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      ) : (
+        <Button onClick={() => setIsAdding(true)}>
+          <PlusIcon /> Add Task
+        </Button>
+      )}
+    </div>
+  );
+}
