8a2ad708f754d1d9d889fe88ffb1c39183f58c45
IvanOrsh <ksironov@mail.ru>
2024-03-08 23:53:54 +0300

    feat: implement complete task server action

---
+++ src/actions/complete-tasks.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { tasks } from "@/lib/schema";
+
+export default async function completeTask(id: number) {
+  const session = await auth();
+
+  if (!session) {
+    return {
+      message: "unauthenticated",
+    };
+  }
+
+  await db
+    .update(tasks)
+    .set({
+      isComplete: true,
+    })
+    .where(and(eq(tasks.id, id), eq(tasks.userId, session.user.id)));
+
+  // don't forget!
+  revalidatePath("/tasks");
+}
