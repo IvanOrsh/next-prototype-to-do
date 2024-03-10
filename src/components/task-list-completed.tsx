90f67f1f2a82482e62e66217e48e7af921dc2432
IvanOrsh <ksironov@mail.ru>
2024-03-10 21:47:25 +0300

    fix: use cn function

--- src/components/task-list-completed.tsx
+++ src/components/task-list-completed.tsx
@@ -6,6 +6,7 @@ import type { Task } from "@/types/task";
 import { Button } from "./ui/button";
 import { ChevronDownIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 import TaskList from "./task-list";
+import { cn } from "@/lib/utils";
 
 type TaskListCompletedProps = {
   tasks: Task[];
@@ -24,7 +25,7 @@ export default function TaskListCompleted({
         <div className="flex flex-col gap-5">
           <div>
             <Button
-              className="text-accent-blue-foreground bg-accent hover:bg-accent/50"
+              className={cn("bg-accent hover:bg-accent/50", accentClassName)}
               onClick={() => setOpen(!open)}
             >
               <ChevronDownIcon className="mr-2" /> Completed
@@ -34,7 +35,7 @@ export default function TaskListCompleted({
         </div>
       ) : (
         <Button
-          className="text-accent-blue-foreground bg-accent hover:bg-accent/50"
+          className={cn("bg-accent hover:bg-accent/50", accentClassName)}
           onClick={() => setOpen(!open)}
         >
           <ChevronRightIcon className="mr-2" /> Completed
