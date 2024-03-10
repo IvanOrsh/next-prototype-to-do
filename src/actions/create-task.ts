ebefd680208b7faa09e782fa7aab85e10454cec3
IvanOrsh <ksironov@mail.ru>
2024-03-10 23:50:23 +0300

    refactory: add type for create task schema

--- src/actions/create-task.ts
+++ src/actions/create-task.ts
@@ -6,7 +6,13 @@ import { auth } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { tasks } from "@/lib/schema";
 
-export async function createTask(data: any) {
+export type CreateTaskSchema = {
+  title: string;
+  isImportant: boolean;
+  addedToMyDayAt?: string;
+};
+
+export async function createTask(data: CreateTaskSchema) {
   const session = await auth();
 
   if (!session) {
@@ -19,6 +25,7 @@ export async function createTask(data: any) {
     userId: session.user.id,
     title: data.title,
     isImportant: data.isImportant,
+    addedToMyDayAt: data.addedToMyDayAt,
   };
 
   await db.insert(tasks).values(dataToInsert);
